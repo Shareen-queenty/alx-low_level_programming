c421aa6d2f3e9144e10a5597a662c6a7d4e453e4
Shareen-queenty <tolex4gold100@gmail.com>
2023-04-23 18:07:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
-
 /**
- * main - A c program that print a line to the standard error
- * Return 1 (Success)
+ * main -A program that print the size
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
-return (1);
-}	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
