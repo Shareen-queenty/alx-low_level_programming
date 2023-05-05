1e757467a35ff3268c6fcdf5073dabde155857e4
Shareen-queenty <tolex4gold100@gmail.com>
2023-05-04 21:49:53 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int j;
+
 j = 0;
 while (j < n && src[j] != '\0')
 {
-dest[j] = src[j]
+dest[j] = src[j];
 j++;
 }
 while (j < n)
 {
-dest[j] = '\0'
+dest[j] = '\0';
 j++;
 }
 return (dest);
