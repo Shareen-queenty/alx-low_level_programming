9cfb0377e5a8bfc479e395a4374392e724d56952
Shareen-queenty <tolex4gold100@gmail.com>
2023-05-15 12:51:56 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
  * Return: the two strings concatenated
  */
 char *str_concat(char *s1, char *s2)
-{       
+{
 int i = 0, j = 0, k = 0, l = 0;
 char *s;
 if (s1 == NULL)
@@ -17,11 +17,11 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 while (s1[i])
-i++;    
+i++;
 while (s2[j])
-j++;    
+j++;
 l = i + j;
-s = malloc((sizeof(char) * l) + 1);
+s = malloc((sizeof(char) * l) +1);
 if (s == NULL)
 return (NULL);
 j = 0;
